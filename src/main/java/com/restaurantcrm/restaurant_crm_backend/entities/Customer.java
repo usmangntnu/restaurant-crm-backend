42054981e882e523fab06a753c2ef17e5fa50613
Usman <usmang@ntnu.no>
2025-08-01 17:26:20 +0200

    Add annotations

--- src/main/java/com/restaurantcrm/restaurant_crm_backend/entities/Customer.java
+++ src/main/java/com/restaurantcrm/restaurant_crm_backend/entities/Customer.java
@@ -1,4 +1,19 @@
 package com.restaurantcrm.restaurant_crm_backend.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@Table(name = "customer")
 public class Customer {
 }
