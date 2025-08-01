f08ef34d216563309b9e8ff3265bbf3079b598ae
Usman <usmang@ntnu.no>
2025-08-01 14:26:46 +0200

    Initial commit: Spring Boot backend project

---
+++ src/main/java/com/restaurantcrm/restaurant_crm_backend/RestaurantCrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.restaurantcrm.restaurant_crm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCrmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantCrmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurantcrm/restaurant_crm_backend/RestaurantCrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantcrm.restaurant_crm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCrmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
